12da05405e2537e4186b5fd97750e489310a30dc
ches <chestnut31415@gmail.com>
2024-10-25 03:27:25 +0900

    記録タイミングが `delay` か `continued` の時、再生開始まで待機

--- src/entrypoints/content/wait.ts
+++ src/entrypoints/content/wait.ts
@@ -4,7 +4,7 @@ import { asyncQuerySelector } from "./async-query-selector";
 export function wait(recordTiming: RecordTiming, ctx: ContentScriptContext) {
 	console.log("recordTiming", recordTiming);
 	if (recordTiming.type === "delay") {
-		return waitDelay(recordTiming.delaySeconds, ctx);
+		return waitDelay(recordTiming.delaySeconds, "video", ctx);
 	}
 	if (recordTiming.type === "ended") {
 		return waitEnded("video");
@@ -15,15 +15,42 @@ export function wait(recordTiming: RecordTiming, ctx: ContentScriptContext) {
 	throw new Error("記録タイミングの値が不正です");
 }
 
-// 再生開始からn秒待機
-function waitDelay(waitSecond: number, ctx: ContentScriptContext) {
-	return new Promise<void>((resolve, reject) => {
-		const timeout = setTimeout(resolve, waitSecond * 1000);
+// 再生開始まで待機
+function waitPlaying(elem: HTMLVideoElement) {
+	return new Promise<void>((resolve) => {
+		if (!elem.paused) {
+			return resolve();
+		}
+		const onPlaying = () => {
+			elem.removeEventListener("playing", onPlaying);
+			return resolve();
+		};
+		elem.addEventListener("playing", onPlaying);
+	});
+}
 
-		// ページ遷移時にクリーンアップ & reject
-		ctx.addEventListener(window, "wxt:locationchange", () => {
-			clearTimeout(timeout);
-			return reject();
+// 再生開始からn秒待機
+function waitDelay(
+	waitSecond: number,
+	selector: string,
+	ctx: ContentScriptContext,
+) {
+	return new Promise<void>((resolve, reject) => {
+		asyncQuerySelector(selector).then((elem) => {
+			if (!elem || !(elem instanceof HTMLVideoElement)) {
+				return reject(new Error("video要素の取得に失敗しました"));
+			}
+
+			// 再生開始まで待機
+			waitPlaying(elem).then(() => {
+				const timeout = setTimeout(resolve, waitSecond * 1000);
+
+				// ページ遷移時にクリーンアップ & reject
+				ctx.addEventListener(window, "wxt:locationchange", () => {
+					clearTimeout(timeout);
+					return reject();
+				});
+			});
 		});
 	});
 }
@@ -57,34 +84,39 @@ function waitContinued(
 				return reject(new Error("video要素の取得に失敗しました"));
 			}
 
-			let startTime = elem.currentTime;
+			// 再生開始まで待機
+			// レジューム再生時に即resolveされるのを防ぐため
+			waitPlaying(elem as HTMLVideoElement).then(() => {
+				let startTime = elem.currentTime;
 
-			const resetTime = () => {
-				startTime = elem.currentTime;
-			};
+				const resetTime = () => {
+					startTime = elem.currentTime;
+				};
 
-			const cleanup = () => {
-				clearInterval(interval);
-				elem.removeEventListener("pause", resetTime);
-				elem.removeEventListener("seeked", resetTime);
-			};
+				const cleanup = () => {
+					clearInterval(interval);
+					elem.removeEventListener("pause", resetTime);
+					elem.removeEventListener("seeked", resetTime);
+				};
 
-			elem.addEventListener("pause", resetTime);
-			elem.addEventListener("seeked", resetTime);
+				elem.addEventListener("pause", resetTime);
+				elem.addEventListener("seeked", resetTime);
 
-			const interval = setInterval(() => {
-				console.log("interval");
-				const playTime = elem.currentTime - startTime;
-				if (playTime > waitSecond) {
+				// 1秒ごとに再生時間をチェック
+				const interval = setInterval(() => {
+					console.log("interval");
+					const playTime = elem.currentTime - startTime;
+					if (playTime > waitSecond) {
+						cleanup();
+						return resolve();
+					}
+				}, 1000);
+
+				// ページ遷移時にクリーンアップ & reject
+				ctx.addEventListener(window, "wxt:locationchange", () => {
 					cleanup();
-					return resolve();
-				}
-			}, 1000);
-
-			// ページ遷移時にクリーンアップ & reject
-			ctx.addEventListener(window, "wxt:locationchange", () => {
-				cleanup();
-				return reject();
+					return reject();
+				});
 			});
 		});
 	});
