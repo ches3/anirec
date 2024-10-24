fe1e854f1dfae85b4450dcacaa30961617fa9231
ches <chestnut31415@gmail.com>
2024-10-25 03:23:43 +0900

    `utils/*` の自動インポートをしない

--- wxt.config.ts
+++ wxt.config.ts
@@ -4,6 +4,11 @@ import { defineConfig } from "wxt";
 export default defineConfig({
 	modules: ["@wxt-dev/module-react"],
 	srcDir: "src",
+	imports: {
+		dirsScanOptions: {
+			fileFilter: () => false,
+		},
+	},
 	manifest: {
 		name: "Annict Recorder",
 		version: "0.1.0",
