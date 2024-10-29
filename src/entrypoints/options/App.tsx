39b97cf133840f620531f2f86e4bd5cb86b66bbb
ches <chestnut31415@gmail.com>
2024-10-29 16:50:32 +0900

    サイトごとに有効/無効を切り替えれるようにする Fixes #5

--- src/entrypoints/options/App.tsx
+++ src/entrypoints/options/App.tsx
@@ -1,10 +1,12 @@
 import { RecordTimingOption } from "./RecordTimingOption";
+import { ServiceToggleOption } from "./ServiceToggleOption";
 import { TokenOption } from "./TokenOption";
 
 function App() {
 	return (
 		<div className="mx-6 mt-2 mb-8 text-base">
 			<TokenOption />
+			<ServiceToggleOption className="mt-6" />
 			<RecordTimingOption className="mt-6" />
 		</div>
 	);
