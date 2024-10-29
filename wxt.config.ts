4bd2b98fa02bb7cb0fbad4f21163dd46c48095ff
ches <chestnut31415@gmail.com>
2024-10-29 16:54:55 +0900

    firefoxに対応

--- wxt.config.ts
+++ wxt.config.ts
@@ -1,3 +1,4 @@
+import type { UserManifest } from "wxt";
 import { defineConfig } from "wxt";
 
 // See https://wxt.dev/api/config.html
@@ -9,10 +10,31 @@ export default defineConfig({
 			fileFilter: () => false,
 		},
 	},
-	manifest: {
-		name: "Annict Recorder",
-		version: "0.1.0",
-		description: "",
-		permissions: ["storage", "identity"],
+	manifest: ({ browser, manifestVersion }) => {
+		const permissions = ["storage", "identity"];
+		if (manifestVersion === 2) {
+			permissions.push("https://api.annict.com/graphql");
+		}
+
+		const browserSpecificSettings: Record<
+			string,
+			UserManifest["browser_specific_settings"]
+		> = {
+			firefox: {
+				gecko: {
+					id: "annict-recorder@ches3.me",
+					strict_min_version: "109.0",
+				},
+			},
+		};
+
+		return {
+			name: "Annict Recorder",
+			version: "0.1.0",
+			description: "",
+			permissions,
+			browser_specific_settings: browserSpecificSettings[browser],
+			a: "a",
+		};
 	},
 });
